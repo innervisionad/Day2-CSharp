735f0802fa8ab2af803675a959388f8d074db611
Adam Clements <aj.clements@live.co.uk>
2016-06-21 16:08:55 +0100

    game Library

---
+++ gameLibrary/gameLibrary/Game.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gameLibrary
+{
+    class Game
+    {
+        public string title;
+        public string genre;
+        public string ageRating;
+        public string platform;
+        public int price;
+
+        public static int totalGames = 0;
+
+
+
+        public Game(string title, string genre, string ageRating, string platform, int price)
+        {
+            this.title = title;
+            this.genre = genre;
+            this.ageRating = ageRating;
+            this.platform = platform;
+            this.price = price;
+            totalGames += 1;
+        }
+        public void sellGame()
+        {
+            Console.WriteLine("We sold {0} for £{1}.", this.title, this.price);
+        }
+        public static void numOfGames()
+        {
+            Console.WriteLine("We have {0} game in stock.", totalGames);
+        }
+
+
+
+
+    }
+
+}
---
+++ gameLibrary/gameLibrary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gameLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game warcraft = new Game("World of Warcraft", "MMORPG", "12+", "PC", 30);
+            Game halo = new Game("Halo", "Shooter", "15", "Xbox", 45);
+            Game uncharted = new Game("uncharted", "Action", "15", "PS4", 45);
+            Game zelda = new Game("Zelda", "RPG", "5+", "Wii", 40);
+
+
+            warcraft.sellGame();
+            halo.sellGame();
+            uncharted.sellGame();
+            zelda.sellGame();
+
+
+
+
+
+
+        }
+    }
+}
