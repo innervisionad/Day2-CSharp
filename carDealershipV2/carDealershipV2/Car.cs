d6dedfeca174ad1b9dc5410b21b0de71c711c7ef
Adam Clements <aj.clements@live.co.uk>
2016-06-21 10:53:58 +0100

    carDealershipV2 with get price instance method

---
+++ carDealershipV2/carDealershipV2/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carDealershipV2
+{
+    class Car
+    {
+        public string name;
+        public string model;
+        public int price;
+
+        public static int totalCars = 0;
+
+        public Car(string name, string model, int price)
+        {
+            this.name = name;
+            this.model = model;
+            this.price = price;
+            totalCars += 1;
+        }
+        public void getPrice()
+        {
+            Console.WriteLine("The price of {0} is {1}", this.name, this.price.ToString());
+        }
+        public static void numOfCars()
+        {
+            Console.WriteLine("We have a total of " + totalCars + " cars.");
+        }
+    }
+}
---
+++ carDealershipV2/carDealershipV2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carDealershipV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Write a non-static (instance) method to add some cars to the program
+            //Add another non-static method
+            //e.g.to list the details of each car or record that a car has been sold(you may need to add a ‘sold’ field to the car class)
+
+            //instantiate 3 cars
+
+            Car mini = new Car("MiniCooper", "hatchback", 10000);
+          
+
+            Car mazda = new Car("mazda", "2", 12000);
+            
+
+            Car peugeot = new Car("peugeot", "407", 15000);
+
+            //program start
+            
+            mini.getPrice();
+            mazda.getPrice();
+            peugeot.getPrice();
+
+            Car.numOfCars();
+        }
+    }
+}
