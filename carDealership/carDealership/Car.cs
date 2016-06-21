fec88c611c403a04d810c5a2a5a2895f5014e7f6
Adam Clements <aj.clements@live.co.uk>
2016-06-21 10:31:03 +0100

    carDealership

---
+++ carDealership/carDealership/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carDealership
+{
+    class Car
+    {
+        public string name;
+        public string model;
+        public int price;
+
+        public static int totalCars = 0;
+
+        public Car(string name, string model, int price)
+        {
+            this.name = name;
+            this.model = model;
+            this.price = price;
+            totalCars += 1;
+        }
+    }
+}
---
+++ carDealership/carDealership/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carDealership
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //You are writing an application for a car dealership.
+            //Create a car class with some non-static fields and a static field to record the total number of cars in the program.
+            //Instantiate 3 objects
+            //Assign values to their properties
+            //Increment the total number of cars each time you instantiate another car object
+            //Display the values of the objects and the static field.
+
+
+            //instantiate 3 cars
+            Car mini = new Car("MiniCooper", "hatchback", 10000);       
+
+            Car mazda = new Car("mazda", "2", 12000);
+          
+            Car peugeot = new Car("peugeot", "407", 15000);
+
+            //program start
+            Console.WriteLine("We have a total of " + Car.totalCars + " cars.");
+
+
+        }
+       
+    }
+}
